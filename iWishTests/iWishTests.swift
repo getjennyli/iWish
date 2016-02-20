d7dad9583a888e88a9e4a4ba7cdb500d84ea45ec
Jiani Li <mapleljn@hotmail.com>
2016-02-20 15:49:06 -0500

    Initial Commit

---
+++ iWishTests/iWishTests.swift
@@ -0,0 +1,36 @@
+//
+//  iWishTests.swift
+//  iWishTests
+//
+//  Created by 卡卡 on 2/20/16.
+//  Copyright © 2016 All-Nighters. All rights reserved.
+//
+
+import XCTest
+@testable import iWish
+
+class iWishTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
