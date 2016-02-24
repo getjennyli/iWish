323b3f90b3ad110dccba8d92806ba2f1616a20c3
Jenny <mapleljn@hotmail.com>
2016-02-23 21:09:50 -0500

    Add/Delete

---
+++ iWish/Wish.swift
@@ -0,0 +1,19 @@
+//
+//  Wish.swift
+//  iWish
+//
+//  Created by 卡卡 on 2/20/16.
+//  Copyright © 2016 All-Nighters. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Wish: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
---
+++ iWish/WishTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WishTableViewCell.swift
+//  iWish
+//
+//  Created by 卡卡 on 2/20/16.
+//  Copyright © 2016 All-Nighters. All rights reserved.
+//
+
+import UIKit
+
+class WishTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
