3ac82f8114bb0483da7e3da15d10e7805bc1b657
Jenny <mapleljn@hotmail.com>
2016-03-03 17:04:16 -0500

    //completedWish

--- iWish/WishListViewController.swift
+++ iWish/WishListViewController.swift
@@ -14,7 +14,7 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var tableView: UITableView!
     var wishs = [Wish]()
     var openWishs : Results<Wish>!
-    var completedWishs : Results<Wish>!
+   // var completedWishs : Results<Wish>!
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -38,7 +38,7 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
             let realm = try Realm()
             
             openWishs = realm.objects(Wish).filter("isCompleted = false")
-            completedWishs = realm.objects(Wish).filter("isCompleted = true")
+          //  completedWishs = realm.objects(Wish).filter("isCompleted = true")
             
             tableView?.reloadData()
         } catch {
@@ -52,33 +52,33 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0{
+        //if section == 0{
             return "Open Wishs"
-        }
-        return "Completed Wishs"
+      //  }
+       // return "Completed Wishs"
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
+        //if section == 0 {
             return openWishs.count
-        }
-        return completedWishs.count
+        //}
+        //return completedWishs.count
     }
 
         func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("WishTableViewCell", forIndexPath: indexPath) as! WishTableViewCell
             var wish: Wish!
-            if indexPath.section == 0 {
+           // if indexPath.section == 0 {
                 wish = openWishs[indexPath.row]
-            }
+           // }
             // let wish = datasource[indexPath.row]
-            else {
-                wish = completedWishs[indexPath.row]
-            }
+           // else {
+           //     wish = completedWishs[indexPath.row]
+           // }
             cell.nameLabel?.text = wish.name
             cell.priceLabel?.text = String(wish.price)
             return cell
@@ -88,12 +88,12 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
         let deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Destructive, title: "Delete") { (deleteAction, indexPath) -> Void in
             
             var wishToBeDeleted : Wish!
-            if indexPath.section == 0 {
+           // if indexPath.section == 0 {
                 wishToBeDeleted = self.openWishs[indexPath.row]
-            }
-            else {
-                wishToBeDeleted = self.completedWishs[indexPath.row]
-            }
+            //}
+            //else {
+            //    wishToBeDeleted = self.completedWishs[indexPath.row]
+            //}
             try! uiRealm.write({ () -> Void in
                 uiRealm.delete(wishToBeDeleted)
                 self.reloadTheTable()
@@ -101,12 +101,12 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
         }
         let doneAction = UITableViewRowAction(style: UITableViewRowActionStyle.Normal, title: "Done") { (doneAction, indexPath) -> Void in
             var wishToBeUpdated : Wish!
-            if indexPath.section == 0 {
+            //if indexPath.section == 0 {
                 wishToBeUpdated = self.openWishs[indexPath.row]
-            }
-            else {
-                wishToBeUpdated = self.completedWishs[indexPath.row]
-            }
+            //}
+            //else {
+            //    wishToBeUpdated = self.completedWishs[indexPath.row]
+           // }
            try! uiRealm.write({ () -> Void in
                 wishToBeUpdated.isCompleted = true
                 self.reloadTheTable()
@@ -121,14 +121,14 @@ class WishListViewController: UIViewController, UITableViewDelegate, UITableView
             
             if let selectedWishCell = sender as? WishTableViewCell {
                 let indexPath = tableView.indexPathForCell(selectedWishCell)
-                if indexPath!.section == 0 {
+              //  if indexPath!.section == 0 {
                     let selectedWish = openWishs[indexPath!.row]
                     wishDetailViewController!.wish = selectedWish
                     
-                } else {
-                    let selectedWish = completedWishs[indexPath!.row]
-                    wishDetailViewController!.wish = selectedWish
-                }
+               // } else {
+               //     let selectedWish = completedWishs[indexPath!.row]
+               //     wishDetailViewController!.wish = selectedWish
+               // }
             }
            
         }
